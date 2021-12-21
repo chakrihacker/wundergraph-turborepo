dddb195a06c70f54336b493cb31129cbd3866a61
Subramanya Chakravarthy <chanty.chakravarthy@gmail.com>
2021-12-21 22:48:09 +0530

    initial commit 🎉

---
+++ apps/xpo/metro.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const getWorkspaces = require('get-yarn-workspaces');
+const workspaces = getWorkspaces(__dirname);
+const pkg = require("./package.json")
+
+const pkgName = pkg.name
+
+module.exports = {
+  // projectRoot: path.resolve(__dirname, 'apps/rnative'),
+
+  watchFolders: [path.resolve(__dirname, 'node_modules'), ...workspaces],
+
+  resolver: {
+    blocklist: workspaces.map(
+      workspacePath =>
+        `/${workspacePath.replace(
+          /\//g,
+          '[/\\\\]',
+        )}[/\\\\]node_modules[/\\\\]react-native[/\\\\].*/`,
+    ),
+
+    extraNodeModules: {
+      'react-native': path.resolve(
+        __dirname,
+        `apps/${pkgName}/node_modules/react-native`,
+      ),
+    },
+  },
+};
